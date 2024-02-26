85cf5b516c063e6fb2d111352b88d11c3301dbb3
Vincent Speelman <vinspee@Vincents-MacBook-Pro.local>
2024-02-26 10:02:08 -0500

    test

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,23 @@
 import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import { routeLoader$, type DocumentHead } from "@builder.io/qwik-city";
+import { geolocation } from "@vercel/edge";
+
+export const useLocationLoader = routeLoader$((event) => {
+  const location = geolocation(event)
+  return location;
+})
 
 export default component$(() => {
+  const locationLoader = useLocationLoader();
   return (
-    <>
-      <h1>Hi 👋</h1>
-      <p>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </p>
-    </>
+    <div>
+      <h1>Your location</h1>
+    <pre>
+    <code>
+      {JSON.stringify(locationLoader.value, null, 2)}
+    </code>
+    </pre>
+    </div>
   );
 });
 
