e2f55e8ae4169853c5700a1348f0f14aa3054632
Vincent Speelman <vinspee@Vincents-MacBook-Pro.local>
2024-03-01 12:17:57 -0500

    force ssr

--- adapters/vercel-edge/vite.config.ts
+++ adapters/vercel-edge/vite.config.ts
@@ -11,6 +11,8 @@ export default extendConfig(baseConfig, () => {
       },
       outDir: ".vercel/output/functions/_qwik-city.func",
     },
-    plugins: [vercelEdgeAdapter()],
+    plugins: [vercelEdgeAdapter({
+      ssg: null,
+    })],
   };
 });
